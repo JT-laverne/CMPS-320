a65f69fb4d7e70e2404cdaac60d743f587eaed9e
JT-laverne <149021764+JT-laverne@users.noreply.github.com>
2025-06-03 19:18:34 -0700

    Delete cmps320 directory

--- cmps320/db_config.php
+++
@@ -1,6 +0,0 @@
-<?php
-$mysqli = new mysqli("localhost", "root", "", "ranking_db");
-if ($mysqli->connect_error) {
-    die("Connection failed: " . $mysqli->connect_error);
-}
-?>
--- cmps320/ranking.sql
+++
@@ -1,9 +0,0 @@
-CREATE DATABASE ranking_db;
-USE ranking_db;
-
-CREATE TABLE ranking (
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(100) NOT NULL,
-  time VARCHAR(50) NOT NULL,
-  platform VARCHAR(50) NOT NULL
-);
